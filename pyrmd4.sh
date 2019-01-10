7d8d606db42fa466f7bcf51b2a59974a8cdbbf79
suchismitarout <suchismitarout47@gmail.com>
2019-01-10 15:06:15 +0530

    pyrmd4

---
+++ pyrmd4.sh
@@ -0,0 +1,26 @@
+for(( i=1;i<=5;i++ ))
+do
+  for(( k=1;k<=5-i;k++ ))
+  do
+     echo -ne " "
+  done
+  for(( j=1;j<=2*i-1;j++ ))
+  do
+     echo -ne "*"
+  done
+  echo
+done
+
+for(( i=4;i>=1;i-- ))
+do 
+  for(( k=1;k<=5-i;k++ ))
+  do
+    echo -ne " "
+  done
+  for(( j=1;j<=2*i-1;j++ ))
+  do
+     echo -ne "*"
+  done
+  echo
+done
+
